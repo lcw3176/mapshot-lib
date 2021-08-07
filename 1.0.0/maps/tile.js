658e594040b2f5238e950bda2bfbd39a59e24ab0
lcw3176 <lcw3176@naver.com>
2021-08-07 12:03:59 +0900

    콜백 추가

--- 1.0.0/maps/tile.js
+++ 1.0.0/maps/tile.js
@@ -7,13 +7,7 @@ class Tile{
         let xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState === xhr.DONE) {
-                // if (xhr.status === 200 || xhr.status === 201) {
-                //     console.log(xhr.responseText);
-                // } else {
-                //     console.error(xhr.responseText);
-                // }
-
-                callback(xhr.responseText);
+                callback(xhr.status, xhr.responseText);
             }
         };
 
