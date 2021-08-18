4e69eced7de7574b8a64183331dd8436db926d80
lcw3176 <lcw3176@naver.com>
2021-08-18 11:55:14 +0900

    withlogo 추가

--- 1.0.0/coors/nfixlat.js
+++ 1.0.0/coors/nfixlat.js
@@ -1,8 +1,12 @@
 class NFixLat{
-    constructor(latlng, naverProfile){
-        let correctFix;
+    constructor(){
         this.width;
-        this.height;
+        this.height; // 로고 없을 때
+        this.logoHeight; // 로고 있을 때
+    }
+
+    generate(latlng, naverProfile){
+        var correctFix;
 
         const controlPoint = 37.5668;
 
@@ -10,13 +14,15 @@ class NFixLat{
             case 16:
                 correctFix = 0.00011633;
                 this.width = 0.01072;
-                this.height = 0.0085;
+                this.height = 0.00829;
+                this.logoHeight = 0.00817;
                 break;
             
             case 18:
                 correctFix = 0.00002833;
                 this.width = 0.00268;
-                this.height = 0.002125
+                this.height = 0.002070; 
+                this.logoHeight = 0.00204;             
                 break;
 
             default:
@@ -24,6 +30,7 @@ class NFixLat{
         }
 
         this.height += (controlPoint - latlng.getY()) * correctFix;
+        this.logoHeight += (controlPoint - latlng.getY()) * correctFix;
     }
 
     getWidthBetweenBlock(){
@@ -32,5 +39,9 @@ class NFixLat{
 
     getHeightBetweenBlock(){
         return this.height;
+    }
+
+    getHeightBetweenBlockWithLogo(){
+        return this.logoHeight;
     }
 }
