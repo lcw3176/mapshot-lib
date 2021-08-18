9bed295825134056da80f9abf74ccea256f964e4
lcw3176 <lcw3176@naver.com>
2021-08-18 11:55:41 +0900

    좌표 함수들 추가

--- 1.0.0/maps/tile.js
+++ 1.0.0/maps/tile.js
@@ -1,17 +1,36 @@
 class Tile{
-    constructor(profile){
-        this.profile = profile;
+    constructor(){
+
     }
 
-    getImage(callback){
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === xhr.DONE) {
-                callback(xhr.status, xhr.responseText);
-            }
-        };
+    getSE(sideBlockCount, NFixLat, coor){
+        var Lat = coor.getY() - NFixLat.getHeightBetweenBlock() * parseInt(sideBlockCount / 2) - NFixLat.getHeightBetweenBlock() / 2;
+        var Lng = coor.getX() + NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) + NFixLat.getWidthBetweenBlock() / 2;
 
-        xhr.open('GET', this.profile.getUrl());
-        xhr.send();
+        return new mapshot.coors.LatLng(Lat, Lng);
+    }
+
+
+    getSW(sideBlockCount, NFixLat, coor){
+        var Lat = coor.getY() - NFixLat.getHeightBetweenBlock() * parseInt(sideBlockCount / 2) - NFixLat.getHeightBetweenBlock() / 2;
+        var Lng = coor.getX() - NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) - NFixLat.getWidthBetweenBlock() / 2;
+
+        return new mapshot.coors.LatLng(Lat, Lng);
+    }
+
+
+    getNE(sideBlockCount, NFixLat, coor){
+        var Lat = coor.getY() + NFixLat.getHeightBetweenBlock() * parseInt(sideBlockCount / 2) + NFixLat.getHeightBetweenBlock() / 2;
+        var Lng = coor.getX() + NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) + NFixLat.getWidthBetweenBlock() / 2;
+
+        return new mapshot.coors.LatLng(Lat, Lng);
+    }
+
+
+    getNW(sideBlockCount, NFixLat, coor){
+        var Lat = coor.getY() + NFixLat.getHeightBetweenBlock() * parseInt(sideBlockCount / 2) + NFixLat.getHeightBetweenBlock() / 2;
+        var Lng = coor.getX() - NFixLat.getWidthBetweenBlock() * parseInt(sideBlockCount / 2) - NFixLat.getWidthBetweenBlock() / 2;
+
+        return new mapshot.coors.LatLng(Lat, Lng);
     }
 }
