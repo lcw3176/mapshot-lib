e594910d1c7ed7e5855dcc7a0b5a61efcb892bcf
joebrooks <lcw3176@naver.com>
2021-12-15 00:28:38 +0900

    POST 추가

--- 1.2.0/maps/kakaoTile.js
+++ 1.2.0/maps/kakaoTile.js
@@ -21,7 +21,7 @@ class KakaoTile{
         xhr.send();
     }
 
-    draw(proxyUrl, onSuccess){
+    drawGet(proxyUrl, onSuccess){
         var kakaoTileOnProgressEvent = new CustomEvent("kakaoTileOnProgress",{
             detail:{
                 percentage:0
@@ -51,5 +51,38 @@ class KakaoTile{
         }
 
         xhr.send();
+    }
+
+    drawPost(proxyUrl, jsonData, onSuccess){
+        var kakaoTileOnProgressEvent = new CustomEvent("kakaoTileOnProgress",{
+            detail:{
+                percentage:0
+            }
+        });
+        
+        var kakaoTileOnErrorEvent = new CustomEvent("kakaoTileOnError");
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', proxyUrl, true);
+
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+        xhr.responseType = 'arraybuffer';
+        
+        xhr.onload = function(e) {            
+            var blob = new Blob([this.response]);
+            onSuccess(blob);
+        };
+
+        xhr.onprogress = function(e) {
+            kakaoTileOnProgressEvent.detail.percentage =  parseInt((e.loaded / e.total) * 100);
+            document.body.dispatchEvent(kakaoTileOnProgressEvent);
+        };
+
+        xhr.onerror = function(){
+            document.body.dispatchEvent(kakaoTileOnErrorEvent);
+        }
+
+        xhr.send(jsonData);
     }
 }
