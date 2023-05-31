45467ef67a28ec6bff79efb3335839222de513c4
lcw3176 <lcw3176@naver.com>
2023-05-31 13:01:43 +0900

    feat: 2.1.0 추가

---
+++ 2.1.0/radius/Radius.js
@@ -0,0 +1,48 @@
+export const Radius = {
+
+    One: {
+        Naver: {
+            sideBlockCount: 11,
+            zoom: 18,
+        },
+        Kakao: {
+            level: 1,
+            width: 5000,
+        }
+
+    },
+    Two: {
+        Naver: {
+            sideBlockCount: 17,
+            zoom: 18,
+        },
+        Kakao: {
+            level: 2,
+            width: 4000,
+        }
+
+    },
+    Five: {
+        Naver: {
+            sideBlockCount: 11,
+            zoom: 16,
+        },
+        Kakao: {
+            level: 5,
+            width: 5000,
+        }
+
+    },
+    Ten: {
+        Naver: {
+            sideBlockCount: 21,
+            zoom: 16,
+        },
+        Kakao: {
+            level: 10,
+            width: 5000,
+        }
+
+    },
+
+};
