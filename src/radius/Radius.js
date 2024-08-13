9936f1b97602ced32b6a011b1f654552e4c8e3d5
lcw3176 <lcw3176@naver.com>
2024-08-13 13:02:35 +0900

    수정해보는중

--- src/radius/Radius.js
+++ src/radius/Radius.js
@@ -12,68 +12,26 @@ export const Radius = {
     // 코드 정리좀
     
     One: {
-        Naver: {
-            sideBlockCount: 11,
-            zoom: 18,
-        },
-        Kakao: {
-            level: 1,
-            // width: 5000,
-        },
-
-        Google:{
-            level:1,
-            // width:6000,
-        }
-
+        sideBlockCount: 11,
+        zoom: 18,
+        level: 1
     },
+
     Two: {
-        Naver: {
-            sideBlockCount: 17,
-            zoom: 18,
-        },
-        Kakao: {
-            level: 2,
-            // width: 4000,
-        },
-
-        Google:{
-            level:2,
-            // width:5000,
-        }
-
+        sideBlockCount: 17,
+        zoom: 18,
+        level: 2
     },
+
     Five: {
-        Naver: {
-            sideBlockCount: 11,
-            zoom: 16,
-        },
-        Kakao: {
-            level: 5,
-            // width: 5000,
-        },
-
-        Google:{
-            level:5,
-            // width:6000,
-        }
-
+        sideBlockCount: 11,
+        zoom: 16,
+        level: 5
     },
     Ten: {
-        Naver: {
-            sideBlockCount: 21,
-            zoom: 16,
-        },
-        Kakao: {
-            level: 10,
-            // width: 5000,
-        },
-
-        Google:{
-            level:10,
-            // width:6000,
-        }
-
+        sideBlockCount: 21,
+        zoom: 16,
+        level: 10,
     },
 
 };
